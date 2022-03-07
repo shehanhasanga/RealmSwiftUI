83bf56a07c634938cb4963cc90f87e330f830505
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-07 07:36:01 +0530

    Initial Commit

---
+++ RealMe/RealMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealMeApp.swift
+//  RealMe
+//
+//  Created by shehan karunarathna on 2022-03-07.
+//
+
+import SwiftUI
+
+@main
+struct RealMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
